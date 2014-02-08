371b43f4c75dfceac496406a981d37424d80f158
Marko Hlebar <marko.hlebar@paddypower.com>
2014-02-08 13:28:41 +0000

    Initial Commit

---
+++ MHFileHandle/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  MHFileHandle
+//
+//  Created by Marko Hlebar on 08/02/2014.
+//  Copyright (c) 2014 Marko Hlebar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
